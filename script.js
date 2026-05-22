7926dc4d0a1cced14676c1701e065a1dbffba460
ichinogroup <dttlinh0210@gmail.com>
2026-05-22 17:41:54 +0900

    Remove PDF link for electrical devices
    Removed the link to the PDF guide for electrical devices.

--- script.js
+++ script.js
@@ -135,7 +135,6 @@ const data = {
         `进入房间前请脱鞋。`
       ],
 
-      ["电器设备", `<a href="assets/pdf/切换指南.pdf" target="_blank">Open PDF Guide</a>`],
       ["空调调节",`<a href="assets/pdf/雫空调.pdf" target="_blank">Open PDF Guide</a>` ],
       ["热水温度", "屋内有一个热水控制面板，安装在洗衣机后面的墙上。<br><br><b>请注意：</b><br><br>◆ 如果误设为 <b>60℃</b>，热水可能无法正常流出，请将温度调低。<br>◆ 如果水不够热或没有变热，请确认设备是否已开启（<b>ON</b>）。<br>◆ 若一段时间未使用，设备可能会自动关闭。<br><br><img src='assets/images/shizuku-hot-water-panel.png' style='width:100%; max-width:320px; border-radius:10px;'>"],
       ["如何丢垃圾", "请将可燃垃圾、罐子/瓶子分类放入有标签的垃圾桶。如果垃圾桶满了，请将垃圾取出，放在房子前门外面，并联系工作人员前来收取。<br><br><img src='assets/images/garbage.png' style='width:100%; max-width:320px; border-radius:10px;'>"],
